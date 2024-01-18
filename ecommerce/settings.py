d31015b24c38d3803e11225b1a57aed90bb299cf
nsviattseva <natalia.sviattseva@gmail.com>
2024-01-18 10:00:05 +0100

    DB Migration testing (to https://railway.app/)

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '-4d13cD3d1EDeCD36E334af6EbDBDe4e',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '12010',
     }
 }
 
