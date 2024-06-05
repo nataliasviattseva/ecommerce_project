bc27776bb4f4450a74061dfef3f085e704ca53a9
nataliasviattseva <30287691+nataliasviattseva@users.noreply.github.com>
2024-06-05 10:04:28 +0200

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&%!0eqa8y^_*tmlkuxqc6th=51!9u2$51xu()_0gn-u7v#mg7*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [f'192.168.187.{i}' for i in range(256)] + ['localhost'] + ['127.0.0.1']
+ALLOWED_HOSTS = [f'192.168.187.{i}' for i in range(256)] + ['localhost'] + ['127.0.0.1'] + ['10.8.50.175']
 
 
 # Application definition
